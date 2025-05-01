77a2911b9cc78c9a6bbec034c2c1fd731afa2672
cumar-dev <cumarcabdicumar2022@gmail.com>
2025-05-01 20:18:20 +0400

    i added files

---
+++ Ternary operator.js
@@ -0,0 +1,9 @@
+age = 20;
+const message = age >= 18 ? "you are an adult" : "you are minor"
+
+console.log(message);
+
+// and operator &&
+
+const me = age >= 18 && "you are an adult"
+console.log(me);
---
+++ else-if.js
@@ -0,0 +1,9 @@
+// let age = 20;
+let age = 18;
+if(age < 13){
+    console.log("you re a child")
+}else if(age < 20){
+    console.log("you are a teenager")
+}else{
+    console.log("you re an adult")
+}
